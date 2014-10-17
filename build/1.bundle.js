4e99b2ad84cbf65e048f012df85087700ace9b7f
Brandon Tilley <brandon@brandontilley.com>
2014-10-17 08:38:37 -0700

    Update bundles

--- build/1.bundle.js
+++ build/1.bundle.js
@@ -1,45 +1,5 @@
-webpackJsonp([1],{
-
-/***/ 3:
-/*!*************************!*\
+webpackJsonp([1],{3:/*!*************************!*\
   !*** ./demo1/demo1.jsx ***!
   \*************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	var React = __webpack_require__(/*! react */ 1);
-	
-	// Create a React component with `React.createClass`.
-	var Hello = React.createClass({
-	  // Every component needs a `render` function that returns one
-	  // concrete (HTML) or composite (custom) component. Components
-	  // representing HTML exist in the `React.DOM` object.
-	  render: function() {
-	    // React components take a hash of properties as their first
-	    // argument and their children as remaining arguments.
-	    // Here, we utilize `this.props`, which is an object
-	    // containing the properties passed to this component.
-	    //
-	    // Properties should be treated as immutable--you should
-	    // not try to change them from inside a component; they
-	    // belong to whoever passed them to you!
-	    return React.DOM.div(null, "Hello ", this.props.name,
-	      React.DOM.strong(null, "!")
-	    );
-	  }
-	});
-	
-	// We can render a component into a DOM node with
-	// `React.renderComponent`, which takes a component
-	// and a DOM node to render into.
-	React.renderComponent(
-	  // We pass `"Minerva"` as the `name` property, which is
-	  // used in `Hello`'s `render` function.
-	  Hello({name: "Minerva"}),
-	  document.getElementById("container")
-	);
-
-
-/***/ }
-
-});
+function(e,o,n){var r=n(/*! react */1),t=r.createClass({render:function(){return r.DOM.div(null,"Hello ",this.props.name,r.DOM.strong(null,"!"))}});r.renderComponent(t({name:"Minerva"}),document.getElementById("container"))}});
 //# sourceMappingURL=1.bundle.js.map
--- build/2.bundle.js
+++ build/2.bundle.js
@@ -1,48 +1,5 @@
-webpackJsonp([2],{
-
-/***/ 4:
-/*!*************************!*\
+webpackJsonp([2],{4:/*!*************************!*\
   !*** ./demo2/demo2.jsx ***!
   \*************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	/** @jsx React.DOM */
-	
-	// JSX is an XML-like syntax that simplifies composing React
-	// components. The @jsx pragma is required at the top of the
-	// file to tell React where the default HTML components live.
-	
-	var React = __webpack_require__(/*! react */ 1);
-	
-	var Hello = React.createClass({displayName: 'Hello',
-	  render: function() {
-	    // Children are composed just like child XML/HTML nodes.
-	    // `{...}` can be used to interpolate raw JavaScript.
-	    return React.DOM.div(null, "Hello ", this.props.name, React.DOM.strong(null, "!"));
-	  }
-	});
-	
-	React.renderComponent(
-	  // Properties look like HTML attributes.
-	  Hello({name: "Minerva"}),
-	  document.getElementById("container")
-	);
-	
-	// JSX compiles directly to JavaScript functions.
-	// That is to say,
-	//
-	//   <Component prop={this.props.name}>
-	//     <strong>Hello</strong>
-	//   </Component>
-	//
-	// is exactly the same as
-	//
-	//   Component({prop: this.props.name},
-	//     React.DOM.strong(null, "Hello")
-	//   )
-
-
-/***/ }
-
-});
+function(e,n,o){var r=o(/*! react */1),t=r.createClass({displayName:"Hello",render:function(){return r.DOM.div(null,"Hello ",this.props.name,r.DOM.strong(null,"!"))}});r.renderComponent(t({name:"Minerva"}),document.getElementById("container"))}});
 //# sourceMappingURL=2.bundle.js.map

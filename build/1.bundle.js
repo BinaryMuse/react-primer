bddab015a147bc3cf97b847584e5419db645fbdb
BinaryMuse <brandon@brandontilley.com>
2015-02-23 08:35:40 -0800

    Update bundles

--- build/1.bundle.js
+++ build/1.bundle.js
@@ -1,75 +1,8 @@
-webpackJsonp([1],{
-
-/***/ 3:
-/*!*************************!*\
+webpackJsonp([1],{3:/*!*************************!*\
   !*** ./demo1/demo1.jsx ***!
   \*************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	var React = __webpack_require__(/*! react */ 1);
-	
-	// Create a ReactComponent class with `React.createClass`. Pass the
-	// class to `React.createFactory` to create a function that, when
-	// called, creates a `ReactElement` object with that type.
-	// React uses ReactElements to create a virtual DOM that determines
-	// how the realm DOM should look and behave.
-	var HelloComponent = React.createClass({displayName: 'HelloComponent',
-	  // Every component needs a `render` function that returns a
-	  // ReactNode. A ReactNode is either:
-	  //
-	  //  * a ReactElement
-	  //  * a string (aka ReactText)
-	  //  * a number (aka ReactText)
-	  //  * an array of ReactNodes (aka ReactFragment)
-	  //
-	  // ReactElement factories that produce ReactElements that represent
-	  // native HTML elements can be found on `React.DOM`.
-	  render: function() {
-	    // ReactElement factories take a hash of properties as their
-	    // first argument and their children as remaining arguments.
-	    // Here, we utilize `this.props`, which is an object
-	    // containing the properties passed to this component.
-	    //
-	    // Properties should be treated as immutable--you should
-	    // not try to change them from inside a component; they
-	    // belong to whoever passed them to you!
-	    return React.DOM.div(null, "Hello ", this.props.name,
-	      React.DOM.strong(null, "!")
-	    );
-	  }
-	});
-	
-	var HelloElementFactory = React.createFactory(HelloComponent);
-	// ReactElement factories are just a convenience for creating new
-	// ReactElements.
-	//
-	//     var element = HelloElementFactory(properties, children...)
-	//
-	// is the same as
-	//
-	//     var element = React.createElement(HelloComponent, properties, children...)
-	
-	// We can render a component into a DOM node with `React.render`,
-	// which takes a ReactElement and a DOM node to render into.
-	React.render(
-	  // We pass `"Minerva"` as the `name` property, which is
-	  // used in `HelloComponent`'s `render` function.
-	  HelloElementFactory({name: "Minerva"}),
-	  document.getElementById("container")
-	);
-
-
-/***/ },
-
-/***/ 10:
-/*!****************************************!*\
+function(e,t,n){var r=n(/*! react */1),a=r.createClass({displayName:"HelloComponent",render:function(){return r.DOM.div(null,"Hello ",this.props.name,r.DOM.strong(null,"!"))}}),o=r.createFactory(a);r.render(o({name:"Minerva"}),document.getElementById("container"))},10:/*!****************************************!*\
   !*** ./~/raw-loader!./demo1/demo1.jsx ***!
   \****************************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = "var React = require(\"react\");\n\n// Create a ReactComponent class with `React.createClass`. Pass the\n// class to `React.createFactory` to create a function that, when\n// called, creates a `ReactElement` object with that type.\n// React uses ReactElements to create a virtual DOM that determines\n// how the realm DOM should look and behave.\nvar HelloComponent = React.createClass({\n  // Every component needs a `render` function that returns a\n  // ReactNode. A ReactNode is either:\n  //\n  //  * a ReactElement\n  //  * a string (aka ReactText)\n  //  * a number (aka ReactText)\n  //  * an array of ReactNodes (aka ReactFragment)\n  //\n  // ReactElement factories that produce ReactElements that represent\n  // native HTML elements can be found on `React.DOM`.\n  render: function() {\n    // ReactElement factories take a hash of properties as their\n    // first argument and their children as remaining arguments.\n    // Here, we utilize `this.props`, which is an object\n    // containing the properties passed to this component.\n    //\n    // Properties should be treated as immutable--you should\n    // not try to change them from inside a component; they\n    // belong to whoever passed them to you!\n    return React.DOM.div(null, \"Hello \", this.props.name,\n      React.DOM.strong(null, \"!\")\n    );\n  }\n});\n\nvar HelloElementFactory = React.createFactory(HelloComponent);\n// ReactElement factories are just a convenience for creating new\n// ReactElements.\n//\n//     var element = HelloElementFactory(properties, children...)\n//\n// is the same as\n//\n//     var element = React.createElement(HelloComponent, properties, children...)\n\n// We can render a component into a DOM node with `React.render`,\n// which takes a ReactElement and a DOM node to render into.\nReact.render(\n  // We pass `\"Minerva\"` as the `name` property, which is\n  // used in `HelloComponent`'s `render` function.\n  HelloElementFactory({name: \"Minerva\"}),\n  document.getElementById(\"container\")\n);\n"
-
-/***/ }
-
-});
+function(e){e.exports='var React = require("react");\n\n// Create a ReactComponent class with `React.createClass`. Pass the\n// class to `React.createFactory` to create a function that, when\n// called, creates a `ReactElement` object with that type.\n// React uses ReactElements to create a virtual DOM that determines\n// how the real DOM should look and behave.\nvar HelloComponent = React.createClass({\n  // Every component needs a `render` function that returns a\n  // ReactNode. A ReactNode is either:\n  //\n  //  * a ReactElement\n  //  * a string (aka ReactText)\n  //  * a number (aka ReactText)\n  //  * an array of ReactNodes (aka ReactFragment)\n  //\n  // ReactElement factories that produce ReactElements that represent\n  // native HTML elements can be found on `React.DOM`.\n  render: function() {\n    // ReactElement factories take a hash of properties as their\n    // first argument and their children as remaining arguments.\n    // Here, we utilize `this.props`, which is an object\n    // containing the properties passed to this component.\n    //\n    // Properties should be treated as immutable--you should\n    // not try to change them from inside a component; they\n    // belong to whoever passed them to you!\n    return React.DOM.div(null, "Hello ", this.props.name,\n      React.DOM.strong(null, "!")\n    );\n  }\n});\n\nvar HelloElementFactory = React.createFactory(HelloComponent);\n// ReactElement factories are just a convenience for creating new\n// ReactElements.\n//\n//     var element = HelloElementFactory(properties, children...)\n//\n// is the same as\n//\n//     var element = React.createElement(HelloComponent, properties, children...)\n\n// We can render a component into a DOM node with `React.render`,\n// which takes a ReactElement and a DOM node to render into.\nReact.render(\n  // We pass `"Minerva"` as the `name` property, which is\n  // used in `HelloComponent`\'s `render` function.\n  HelloElementFactory({name: "Minerva"}),\n  document.getElementById("container")\n);\n'}});
 //# sourceMappingURL=1.bundle.js.map
--- build/3.bundle.js
+++ build/3.bundle.js
@@ -1,81 +1,8 @@
-webpackJsonp([3],{
-
-/***/ 5:
-/*!*************************!*\
+webpackJsonp([3],{5:/*!*************************!*\
   !*** ./demo3/demo3.jsx ***!
   \*************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	var React = __webpack_require__(/*! react */ 1);
-	
-	var Timer = React.createClass({displayName: 'Timer',
-	  // In addition to properties passed from parents, React
-	  // components can also contain an internal state, referenced
-	  // by `this.state`. `getInitialState` defines the value of
-	  // `this.state` when the component is first mounted.
-	  getInitialState:function() {
-	    return {
-	      time: 0
-	    };
-	  },
-	
-	  // React provides lifecycle hooks to set up and tear
-	  // down components and third-party APIs if necessary.
-	  // Here, we start a timer when the component mounts...
-	  componentDidMount:function() {
-	    this.timer = setInterval(this.tick, 1000);
-	  },
-	
-	  // ... and remove the timer when the component unmounts.
-	  // If we don't do this, we'll leak memory (and the timer
-	  // will continue to fire).
-	  componentWillUnmount:function() {
-	    this.timer && clearInterval(this.timer);
-	  },
-	
-	  // Unlike properties, state is owned by the component, and
-	  // can be changed using a few methods. Note that you should
-	  // *never* modify `this.state` directly--always use an API
-	  // method to mutate the state.
-	  //
-	  // Our timer calls `setState`, which allows us to update one or
-	  // more keys in our state. The object passed to `setState` is
-	  // merged with the current state; we can use `replaceState` instead
-	  // if we don't want to merge.
-	  tick:function() {
-	    // Note that React auto-binds all top-level methods in the
-	    // object passed to `createClass` to the component instance.
-	    // Because of this, `this` inside the `tick` method is
-	    // automatically the component instance, and we didn't have
-	    // to use `this.tick.bind(this)` inside the `setInterval` call.
-	    this.setState({time: this.state.time + 1});
-	  },
-	
-	  // Render is called automatically when a component's state is
-	  // updated with `setState`, `replaceState`, or when `forceUpdate`
-	  // is called. Composed components are automatically re-rendered.
-	  render:function() {
-	    return React.createElement("div", null, "Time: ", this.state.time);
-	  }
-	});
-	
-	React.render(
-	  React.createElement(Timer, null),
-	  document.getElementById("container")
-	);
-
-
-/***/ },
-
-/***/ 12:
-/*!****************************************!*\
+function(e,t,n){var a=n(/*! react */1),r=a.createClass({displayName:"Timer",getInitialState:function(){return{time:0}},componentDidMount:function(){this.timer=setInterval(this.tick,1e3)},componentWillUnmount:function(){this.timer&&clearInterval(this.timer)},tick:function(){this.setState({time:this.state.time+1})},render:function(){return a.createElement("div",null,"Time: ",this.state.time)}});a.render(a.createElement(r,null),document.getElementById("container"))},12:/*!****************************************!*\
   !*** ./~/raw-loader!./demo3/demo3.jsx ***!
   \****************************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = "var React = require(\"react\");\n\nvar Timer = React.createClass({\n  // In addition to properties passed from parents, React\n  // components can also contain an internal state, referenced\n  // by `this.state`. `getInitialState` defines the value of\n  // `this.state` when the component is first mounted.\n  getInitialState() {\n    return {\n      time: 0\n    };\n  },\n\n  // React provides lifecycle hooks to set up and tear\n  // down components and third-party APIs if necessary.\n  // Here, we start a timer when the component mounts...\n  componentDidMount() {\n    this.timer = setInterval(this.tick, 1000);\n  },\n\n  // ... and remove the timer when the component unmounts.\n  // If we don't do this, we'll leak memory (and the timer\n  // will continue to fire).\n  componentWillUnmount() {\n    this.timer && clearInterval(this.timer);\n  },\n\n  // Unlike properties, state is owned by the component, and\n  // can be changed using a few methods. Note that you should\n  // *never* modify `this.state` directly--always use an API\n  // method to mutate the state.\n  //\n  // Our timer calls `setState`, which allows us to update one or\n  // more keys in our state. The object passed to `setState` is\n  // merged with the current state; we can use `replaceState` instead\n  // if we don't want to merge.\n  tick() {\n    // Note that React auto-binds all top-level methods in the\n    // object passed to `createClass` to the component instance.\n    // Because of this, `this` inside the `tick` method is\n    // automatically the component instance, and we didn't have\n    // to use `this.tick.bind(this)` inside the `setInterval` call.\n    this.setState({time: this.state.time + 1});\n  },\n\n  // Render is called automatically when a component's state is\n  // updated with `setState`, `replaceState`, or when `forceUpdate`\n  // is called. Composed components are automatically re-rendered.\n  render() {\n    return <div>Time: {this.state.time}</div>;\n  }\n});\n\nReact.render(\n  <Timer />,\n  document.getElementById(\"container\")\n);\n"
-
-/***/ }
-
-});
+function(e){e.exports="var React = require(\"react\");\n\nvar Timer = React.createClass({\n  // In addition to properties passed from parents, React\n  // components can also contain an internal state, referenced\n  // by `this.state`. `getInitialState` defines the value of\n  // `this.state` when the component is first mounted.\n  getInitialState() {\n    return {\n      time: 0\n    };\n  },\n\n  // React provides lifecycle hooks to set up and tear\n  // down components and third-party APIs if necessary.\n  // Here, we start a timer when the component mounts...\n  componentDidMount() {\n    this.timer = setInterval(this.tick, 1000);\n  },\n\n  // ... and remove the timer when the component unmounts.\n  // If we don't do this, we'll leak memory (and the timer\n  // will continue to fire).\n  componentWillUnmount() {\n    this.timer && clearInterval(this.timer);\n  },\n\n  // Unlike properties, state is owned by the component, and\n  // can be changed using a few methods. Note that you should\n  // *never* modify `this.state` directly--always use an API\n  // method to mutate the state.\n  //\n  // Our timer calls `setState`, which allows us to update one or\n  // more keys in our state. The object passed to `setState` is\n  // merged with the current state; we can use `replaceState` instead\n  // if we don't want to merge.\n  tick() {\n    // Note that React auto-binds all top-level methods in the\n    // object passed to `createClass` to the component instance.\n    // Because of this, `this` inside the `tick` method is\n    // automatically the component instance, and we didn't have\n    // to use `this.tick.bind(this)` inside the `setInterval` call.\n    this.setState({time: this.state.time + 1});\n  },\n\n  // Render is called automatically when a component's state is\n  // updated with `setState`, `replaceState`, or when `forceUpdate`\n  // is called. Composed components are automatically re-rendered.\n  render() {\n    return <div>Time: {this.state.time}</div>;\n  }\n});\n\nReact.render(\n  <Timer />,\n  document.getElementById(\"container\")\n);\n"}});
 //# sourceMappingURL=3.bundle.js.map
